baa096e7088aa882df058e3d58d7855e916532f0
gugucazhiz <domateklh@gmail.com>
2026-03-17 17:18:25 -0300

    Dark and light using cookies

---
+++ pw2/src/test/java/br/com/ufrn/tads/pw2/Pw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufrn.tads.pw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
