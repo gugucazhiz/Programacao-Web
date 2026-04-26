11d49abf7d0a9c5a6c19034eb7068a0c61975c61
gugucazhiz <domateklh@gmail.com>
2026-04-26 19:09:13 -0300

    feat: working with data biding

---
+++ unidade2/aula1/src/main/java/ufrn/br/webmvcapp/domain/User.java
@@ -0,0 +1,13 @@
+package ufrn.br.webmvcapp.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+    private String firstName;
+    private String lastName;
+}
