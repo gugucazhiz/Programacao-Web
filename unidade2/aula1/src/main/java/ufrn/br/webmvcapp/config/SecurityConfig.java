2184c089f0e9156c7a05aee3e47bd1431eac4684
gugucazhiz <domateklh@gmail.com>
2026-05-12 11:11:10 -0300

    feat: new security filer chain logic

--- unidade2/aula1/src/main/java/ufrn/br/webmvcapp/config/SecurityConfig.java
+++ unidade2/aula1/src/main/java/ufrn/br/webmvcapp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ufrn.br.webmvcapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,10 +11,33 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // 1. DEFINIÇÃO DE AUTORIZAÇÃO DE ROTAS
+                .authorizeHttpRequests(authz -> authz
+                        // Regra Específica: A pasta de CSS é pública (Allowlist)
+                        .requestMatchers("/css/**").permitAll()
+                        .requestMatchers("/users/create").hasRole("ADMIN")
+                        // Regra Geral (Catch-All): Todas as demais exigem autenticação
+                        .anyRequest().authenticated()
+                )
+                // 2. DEFINIÇÃO DE AUTENTICAÇÃO
+                // Pede ao Spring para usar proteção via Sessão Web e gerar a página de Login Padrão
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/mvc/tarefas", true)
+                        .permitAll()
+                );
+        return http.build();
+    }
+
     @Bean
     public UserDetailsService userDetailsService(PasswordEncoder encoder) {
         // Constrói um usuário padrão do Spring
